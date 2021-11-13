e696fc6ef77234ad5dd4a1c0c41324e476c4d5a4
WILLIAM DE SA RODRIGUES <wsr2k2@gmail.com>
2021-11-13 20:42:01 -0300

    ss

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ const Conn = require("./model/conn/index"); //importando a conexao
 
 Conn(); //executa a func de conexao
 
-app.get("/", (req, res) => {
-    res.status(200).json({ message: "Pagina inicial carregada com sucesso" });
-  });
+// app.get("/", (req, res) => {
+//     res.status(200).json({ message: "Pagina inicial carregada com sucesso" });
+//   });
   
 const usuariosRouter = require("./routers/usuarios.routes");
-app.use('/usuarios',usuariosRouter);
+app.use('/',usuariosRouter);
 
 app.listen(process.env.PORT, () => {
   console.log(`Servidor rodando em: http://localhost:${process.env.PORT}`);
